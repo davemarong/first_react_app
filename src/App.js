28fc3ee762e1d22a004caa09efaf92f33e84b606
Dave Marong <kmarong@hotmail.com>
2021-03-05 20:06:37 +0100

    Start the process of theme context
    - Learning to use theme context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./counter.js";
 import CounterHooks from "./counterHooks.js";
 import Pokemon from "./Pokemon.js";
 
+export const themeContext = React.createContext();
+
 function App() {
+  const [theme, setTheme] = useState("red");
   return (
     <>
-      <Counter startCount={4} name={"Dave"} />
-      <div></div>
-      <Pokemon
-        pikachu={"Nope..."}
-        charizard={"Nope..."}
-        blastoise={"Nope..."}
-      />
-      <div></div>
-      <CounterHooks startCount={0} name={"bro"} />
+      <themeContext.Provider value={{ backgroundColor: theme }}>
+        <Counter startCount={4} name={"Dave"} />
+        <div></div>
+        <Pokemon
+          pikachu={"Nope..."}
+          charizard={"Nope..."}
+          blastoise={"Nope..."}
+        />
+        <div></div>
+        <CounterHooks startCount={0} name={"bro"} />
+        <button
+          onClick={() => {
+            return setTheme();
+          }}
+        ></button>
+      </themeContext.Provider>
     </>
   );
 }
