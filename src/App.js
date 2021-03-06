664d1476ad51c4abbfe26ac9fd49df11906c5369
Dave Marong <kmarong@hotmail.com>
2021-03-06 06:12:59 +0100

    Delete project and start netlify
    - Delete the counter app and start a brand new app(with netlify)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,4 @@
-import React, { useState } from "react";
-import Counter from "./counter.js";
-import CounterHooks from "./counterHooks.js";
-import Pokemon from "./Pokemon.js";
-
-export const themeContext = React.createContext();
-
-function App() {
-  const [theme, setTheme] = useState("red");
-  return (
-    <>
-      <themeContext.Provider value={{ backgroundColor: theme }}>
-        <Counter startCount={4} name={"Dave"} />
-        <div></div>
-        <Pokemon
-          pikachu={"Nope..."}
-          charizard={"Nope..."}
-          blastoise={"Nope..."}
-        />
-        <div></div>
-        <CounterHooks startCount={0} name={"bro"} />
-        <button
-          onClick={() => {
-            return setTheme((prevTheme) => {
-              return prevTheme === "red" ? "blue" : "red";
-            });
-          }}
-        >
-          Toggle theme
-        </button>
-      </themeContext.Provider>
-    </>
-  );
-}
+import React from "react";
+function App() {}
 
 export default App;
